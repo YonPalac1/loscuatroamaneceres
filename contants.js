09740519c4dfc797f32e045dd88613c245c4304a
YonPalac1 <yon.palac@gmail.com>
2025-01-10 13:16:37 -0300

    agrego habitaciones

--- contants.js
+++ contants.js
@@ -8,6 +8,20 @@ import img7 from "./src/assets/galery/img7.png"
 import img8 from "./src/assets/galery/img8.png"
 import img9 from "./src/assets/galery/img9.png"
 
+import am1 from "./src/assets/amanecer/am1.png"
+import am2 from "./src/assets/amanecer/am2.png"
+import am3 from "./src/assets/amanecer/am3.png"
+import am4 from "./src/assets/amanecer/am4.png"
+
+import at1 from "./src/assets/atardecer/at1.png"
+
+import bo1 from "./src/assets/bosque/bo1.png"
+import bo2 from "./src/assets/bosque/bo2.png"
+import bo3 from "./src/assets/bosque/bo3.png"
+import bo4 from "./src/assets/bosque/bo4.png"
+import bo5 from "./src/assets/bosque/bo5.png"
+import bo6 from "./src/assets/bosque/bo6.png"
+
 export const ITEMS = [{
         title: "Habitaciones",
         description: "Sector Campo: 10 Habitaciones cuádruples. Sector Bosque: 8 Habitaciones triples. Sector Piscina: 5 habitaciones cuádruples. Sector Spa: 5 Habitaciones triples/cuádruples. (Todas con baños privados, aire acondicionado y calefacción)."
@@ -22,3 +36,6 @@ export const ITEMS = [{
         description: "Sector Campo: 10 Habitaciones cuádruples. Sector Bosque: 8 Habitaciones triples. Sector Piscina: 5 habitaciones cuádruples. Sector Spa: 5 Habitaciones triples/cuádruples. (Todas con baños privados, aire acondicionado y calefacción)."
 }]
 export const GALERY = [img1, img2,img3,img4,img5,img6,img7,img8,img9]
+export const AMANECER = [am1, am2, am3, am4]
+export const ATARDECER = [at1]
+export const BOSQUES = [bo1, bo2, bo3, bo4, bo5, bo6]
--- src/components/Rooms.jsx
+++ src/components/Rooms.jsx
@@ -1,10 +1,81 @@
+import { Reveal } from "./Reveal";
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import "swiper/css";
+import "swiper/css/autoplay";
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+import { ATARDECER, AMANECER, BOSQUES } from "../../contants";
+import { useState } from "react";
+
+
 const Rooms = ({ bullet }) => {
+    const [rooms, setRooms] = useState(AMANECER)
+    
     return <section className="rooms section-2"
-    id="espacios"
-    style={{
-        display: bullet === 2 ? "flex" : "none"
-    }}
->
+        id="espacios"
+        style={{
+            display: bullet === 2 ? "flex" : "none"
+        }}
+    >
+        <div className="title">
+            <Reveal delay={0.5}>
+                <h2>Habitaciones</h2>
+            </Reveal>
+            <Reveal delay={1}>
+                <p >En Los  Cuatro Amaneceres ofrecemos habitaciones equipadas con baño privado, calefacción y aire acondicionado. Todas con una espectacular vista a nuestros espacios verdes. Nuestra capacidad máxima es de hasta 126 plazas.</p>
+            </Reveal>
+        </div>
+        <div className="filters">
+            <button onClick={() => setRooms(AMANECER)} className="button-filter">Amaneceres</button>
+            <button onClick={() => setRooms(BOSQUES)} className="button-filter">Bosque</button>
+            <button onClick={() => setRooms(ATARDECER)} className="button-filter">Atardecer</button>
+        </div>
+        <div className="container-photos">
+            <Swiper
+                autoplay
+                spaceBetween={0}
+                pagination={{ clickable: true }}
+                slidesPerView={4}
+                breakpoints={{
+                    300: {
+                        slidesPerView: 2,
+                        spaceBetween: 10,
+                    },
+
+                    382: {
+                        slidesPerView: 2,
+                        spaceBetween: 10,
+                    },
+                    400: {
+                        slidesPerView: 2,
+                        spaceBetween: 15,
+                    },
+                    640: {
+                        slidesPerView: 3,
+                        spaceBetween: 10,
+                    },
+                    768: {
+                        slidesPerView: 4,
+                        spaceBetween: 40,
+                    },
+                    1024: {
+                        slidesPerView: 4,
+                        spaceBetween: 50,
+                    },
+                }}
+            >
+                {
+                    rooms.map((img, i) => (
+                        <SwiperSlide key={i}>
+                            <img className="img-slider" src={img}></img>
+                        </SwiperSlide>
+                    ))
+                }
+            </Swiper>
+        </div>
     </section>
 }
 
