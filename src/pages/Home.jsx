8e9fc50e9b52c41e6bb5a91ac6ea7efdcf7e4507
YonPalac1 <yon.palac@gmail.com>
2025-02-06 11:18:08 -0300

    quito el calendario

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,7 +29,7 @@ const Home = () => {
         <Encuentros bullet={bullet} setBullet={setBullet} vid={vid} setVid={setVid} setImageView={setImageView} setImageActive={setImageActive} n={3}/>,
         <Services bullet={bullet} setBullet={setBullet} n={4}/>,
         // <Events bullet={bullet} setBullet={setBullet} n={5} />,
-        <Contact bullet={bullet} setBullet={setBullet} n={6}/>
+        <Contact bullet={bullet} setBullet={setBullet} n={5}/>
     ];
 
     const [images] = useState([vid1, vid2, vid2, vid3, vid4, vid5, vid6, vid7]);
